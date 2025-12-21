3db7e72e70d4cc75a2cd5e0af78328167fa322ae
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-12-21 13:04:31 +1000

    Make the scene group reference property drawer fit in better

--- Modules/SceneManager/Editor/SceneGroupReferencePropertyDrawer.cs
+++ Modules/SceneManager/Editor/SceneGroupReferencePropertyDrawer.cs
@@ -14,20 +14,15 @@ namespace CookieUtils.Extras.SceneManager.Editor
             SerializedProperty name = property.FindPropertyRelative("name");
             ScenesSettings data = ScenesSettings.Get();
 
-            VisualElement layout = new()
-            {
-                style =
-                {
-                    flexDirection = FlexDirection.Row,
-                    paddingLeft = new StyleLength(Length.Pixels(4)),
-                },
-            };
+            VisualElement layout = new() { style = { flexDirection = FlexDirection.Row } };
 
             DropdownField dropdown = new()
             {
                 value = name.stringValue,
-                style = { width = new StyleLength(Length.Percent(50)) },
+                label = property.displayName,
+                style = { flexGrow = 1f },
             };
+            dropdown.AddToClassList("unity-base-field__aligned");
 
             UpdateChoices();
 
@@ -41,13 +36,6 @@ namespace CookieUtils.Extras.SceneManager.Editor
 
             Button edit = new(ScenesSettings.OpenWindow) { text = "Edit" };
 
-            layout.Add(
-                new Label(property.displayName)
-                {
-                    style = { unityTextAlign = TextAnchor.MiddleLeft },
-                }
-            );
-            layout.Add(new VisualElement { style = { flexGrow = 1f } });
             layout.Add(dropdown);
             layout.Add(edit);
             root.Add(layout);
